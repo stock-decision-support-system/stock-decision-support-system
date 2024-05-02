39c609ce2b3842773551de3eb0681ddde2fc201f
Fiorainnnn <11046029@ntub.edu.tw>
2024-05-02 12:35:33 +0800

    新增 - 新增銀行資料dialog

--- frontend/src/components/bankDialog.js
+++ frontend/src/components/bankDialog.js
@@ -1,25 +1,86 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, DatePicker, Input, Select } from 'antd';
+
+const { Option } = Select;
+
 const BankDialog = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
+
   const showModal = () => {
     setIsModalOpen(true);
   };
+
+  const handleNext = () => {
+    if (currentStep === 1) {
+      setCurrentStep(2);
+    }
+  };
+
   const handleOk = () => {
-    setIsModalOpen(false);
+    if (currentStep === 2) {
+      setIsModalOpen(false);
+      setCurrentStep(1);
+    }
   };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+
   return (
     <>
       <Button type="primary" onClick={showModal}>
         新增銀行資料
       </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Modal
+        title={<h2>使用者資料</h2>}
+        visible={isModalOpen}
+        footer={[
+          <Button
+            type="primary"
+            onClick={currentStep === 1 ? handleNext : handleOk}
+          >
+            {currentStep === 1 ? '下一步' : '確定'}
+          </Button>,
+        ]}
+      >
+        <div className="form-container">
+          {currentStep === 1 && (
+            <>
+              <Input placeholder="姓名" className="mt-2" />
+              <Input placeholder="身份證字號" className="mt-1" />
+              <DatePicker placeholder="yyyy/mm/dd" className="w-100 mt-1" />
+              <Select defaultValue="城市" className="mt-1 w-100">
+                <Option value="taipei">台北市</Option>
+                <Option value="newtaipei">新北市</Option>
+              </Select>
+              <Select defaultValue="區" className="mt-1 w-100">
+                <Option value="xx">xx區</Option>
+                <Option value="oo">oo區</Option>
+              </Select>
+              <Input placeholder="地址" className="mt-1" />
+              <Input placeholder="郵遞區號" className="mt-1" />
+            </>
+          )}
+          {currentStep === 2 && (
+            <>
+              <div className="mt-2">銀行名稱</div>
+              <Select defaultValue="請選擇" className="mt-1 w-100">
+                <Option value="taipei">台北市</Option>
+                <Option value="newtaipei">新北市</Option>
+              </Select>
+              城市
+              <Select defaultValue="請選擇" className="mt-1 w-100">
+                <Option value="xx">xx區</Option>
+                <Option value="oo">oo區</Option>
+              </Select>
+              分行名稱
+              <Select defaultValue="請選擇" className="mt-1 w-100">
+                <Option value="taipei">台北市</Option>
+                <Option value="newtaipei">新北市</Option>
+              </Select>
+              <Input placeholder="銀行戶名" className="mt-1" />
+              <Input placeholder="銀行帳號" className="mt-1" />
+            </>
+          )}
+        </div>
       </Modal>
     </>
   );
---
+++ frontend/src/components/bankItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../assets/css/BankDialog.css';
+import { Button, Modal } from 'antd';
+
+class BankItem extends React.Component {
+  render() {
+    const { bankName, fullName, region, branch, account, onModify, onDelete } = this.props;
+
+    return (
+      <div className="bank-item">
+        <div className="bank-info">
+          <h3>銀行名稱: {bankName}</h3>
+          <div className="detail-left">
+            <p>全名: {fullName}</p>
+            <p>地區: {region}</p>
+            <p>分行: {branch}</p>
+          </div>
+          <div className="detail-right">
+            <p>帳號末四碼: {account}</p>
+          </div>
+        </div>
+        <div className="button-group">
+          <Button onClick={onModify}>修改</Button>
+          <Button onClick={onDelete}>刪除</Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BankItem;
