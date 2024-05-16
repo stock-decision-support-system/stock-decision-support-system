a9f07404e06e5b571dac6905771d17630324fdd4
11046003 <11046003@ntub.edu.tw>
2024-05-16 09:15:47 +0800

    新增 - serializers

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import CustomUser, Accounting, ConsumeType
+from .models import CustomUser, Accounting, ConsumeType, APICredentials, InvestmentPortfolio, Investment, Asset, \
+    Liability, Transaction, Budget
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -19,3 +20,45 @@ class AccountingSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class APICredentialsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = APICredentials
+        fields = '__all__'
+
+
+class InvestmentPortfolioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InvestmentPortfolio
+        fields = '__all__'
+
+
+class InvestmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Investment
+        fields = '__all__'
+
+
+class AssetSerializer(serializers.ModelSerializer):
+    transactions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Asset
+        fields = '__all__'
+
+
+class LiabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Liability
+        fields = '__all__'
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = '__all__'
